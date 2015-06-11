3e7472b5c30785a5bbccdd10a1c1505efc7721aa
hua <mzhua78@hotmail.com>
2015-06-11 19:34:20 +0800

    Initial Commit

---
+++ Friday/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Friday
+//
+//  Created by hua on 15/6/11.
+//  Copyright (c) 2015年 wonders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Friday/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Friday
+//
+//  Created by hua on 15/6/11.
+//  Copyright (c) 2015年 wonders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
